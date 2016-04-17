a0a3b71269c7b08959fc2fd0aaf6399d9992f417
Gabor Urbanics <gabor.urbanics@gmail.com>
2016-04-17 21:18:08 +0200

    minor improvements, added demo skeleton points

--- scripts/matplotlib_usage.py
+++ scripts/matplotlib_usage.py
@@ -1,10 +1,29 @@
 # -*- coding: utf-8 -*-
+
+## IMPORT matplotlib and check if it's interactive
+import matplotlib
+matplotlib.is_interactive()
+matplotlib.interactive(True)
+
+## let see what backend we use
+%matplotlib
+
+## these are the usual imports
+import matplotlib.pyplot as plt
 import numpy as np
 
+# get some dummy data
+x = np.arange(-5, 5, .2)
+y = np.sin(x)
 
 
+## state-machine like behavior (MATLAB)
+plt.figure()
 
-import matplotlib.pyplot as plt
+plt.figure(2, figsize=(4, 3))
 
+plt.gcf()
 
+plt.gca()
 
+plt.subplot(2,3,1)
